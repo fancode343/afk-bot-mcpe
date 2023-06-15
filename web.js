92dc8f2f603415ed4f69f237d5471c3f30a4c4ce
fancode343 <118608942+fancode343@users.noreply.github.com>
2023-06-15 20:02:40 +0800

    Update web.js

--- web.js
+++ web.js
@@ -1,11 +1,9 @@
-const express = require("express")
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('<h1>Bot is online</h1> <h2>BY REPLIT</h2> <iframe width="560" height="315" src="https://www.youtube.com/embed/XvQxIsz30rE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> <br> <iframe width="560" height="315" src="https://www.youtube.com/embed/9tLglpFAyIg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+// Serve static files from the "public" folder
+app.use(express.static("public"));
 
-})
-
-    app.listen(3000, () => {
-  console.log('✅Web is up');
+app.listen(3000, () => {
+  console.log('✅ Web is up');
 });
