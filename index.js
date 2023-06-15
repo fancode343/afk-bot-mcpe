91d92ab18edb8c2f507d8ae031cbe5daf8f91671
fancode343 <118608942+fancode343@users.noreply.github.com>
2023-06-15 19:11:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const client = bedrock.createClient({
 host: 'MoonLightSMPG2.aternos.me', 
 port: 64319, 
 version: '1.20.0', 
-username: 'Bot1', 
+username: 'Bot3', 
 offline: true 
 }) 
 console.log ('Connected') 
