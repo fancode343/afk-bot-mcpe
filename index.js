e24503341d1e3b205a69ffb6a84b3449e7442f2f
fancode343 <118608942+fancode343@users.noreply.github.com>
2023-06-16 16:26:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,19 @@ const app = express();
 // Serve static files from the "public" folder
 app.use(express.static("public"));
 
-app.listen(3000, () => {
-  console.log('✅ Web is up');
-});
-
+// Define a route for "/bot"
+app.get("/bot", (req, res) => {
 const bedrock = require('bedrock-protocol') 
 const client = bedrock.createClient({ 
 host: 'MoonLightSMPG2.aternos.me', 
 port: 64319, 
 version: '1.20.0', 
 username: 'Bot3', 
-offline: false
+offline: true
 }) 
-console.log ('Connected') 
+
+});
+
+app.listen(3000, () => {
+  console.log('✅ Web is up');
+});
