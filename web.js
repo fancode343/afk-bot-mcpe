e1c21d7745f2c262ba8e1b04acdf68eed22d2cf6
fancode343 <118608942+fancode343@users.noreply.github.com>
2023-06-17 21:45:41 +0800

    Delete web.js

--- web.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-// Serve static files from the "public" folder
-app.use(express.static("public"));
-
-app.listen(3000, () => {
-  console.log('✅ Web is up');
-});
